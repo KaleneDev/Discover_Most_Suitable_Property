8742aaa8fe1f6d6cba88ac7448f96448e806f0eb
kaleneDev <apiou.kalene@gmail.com>
2023-05-07 23:35:20 +0200

    finish header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <section className="header-warpper">
         <div className="paddings innerWidth flexCenter header-container">
           <img className="logo" src="./images/logo.png" alt="logo" />
-          <div>
+          <div className="flexCenter header-menu">
             <a href="">Résidences</a>
             <a href="">Notre valeur</a>
             <a href="">Contactez-nous</a>
