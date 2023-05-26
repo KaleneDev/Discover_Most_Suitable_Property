2c1c7cd82d17cd66efcab1fae401567ae814b42a
kaleneDev <apiou.kalene@gmail.com>
2023-05-26 19:55:34 +0200

    up

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [react()],
     build: {
         rollupOptions: {
-            external: ["swiper/react"],
+            external: ["Swiper/react"],
         },
     },
     base: "./",
