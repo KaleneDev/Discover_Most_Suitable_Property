73ef554ad198afec605d2e952a9f4363f13fc004
kaleneDev <apiou.kalene@gmail.com>
2023-05-26 19:28:59 +0200

    change path swiper rollupOption

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [react()],
     build: {
         rollupOptions: {
-            external: ["Swiper/react"],
+            external: ["./Swiper/react"],
         },
     },
     base: "./",
